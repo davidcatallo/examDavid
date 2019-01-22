5ff51f3464eb34fd073879cd1e4553db945764b6
catallo david <=>
2019-01-22 13:58:24 +0100

    add require helper for save

--- save.php
+++ save.php
@@ -1,20 +1,8 @@
-<?php 
+<?php require_once('helper.php'); 
 
 
 
-
-$host       = 'localhost';  // Hôte de la base de données
-$dbname     = 'immobilier_david';  // Nom de la bdd
-$port       = '3308';       // Ou 3308 selon la configuration
-$login      = 'root';       // Par défaut dans WAMP
-$password   = '';           // Par défaut dans WAMP (pour MAMP : 'root')
-try {
-    $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8;port='.$port, $login, $password);
-}
-catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-
+$bdd = dbConnect();
 
 $query = "  INSERT INTO logement_david(titre, adresse, ville, cp, surface, prix, photo, type, description)
                 VALUES (:titre, :adresse, :ville, :cp, :surface, :prix, :photo, :type, :description)";
